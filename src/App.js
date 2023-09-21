2bd5ce77790eba6fdf0ed16215c08c41a43b99a3
muhammadrehmankhattak <136075834+muhammadrehmankhattak@users.noreply.github.com>
2023-09-21 16:26:39 +0500

    Final
    Final File

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Nav from './Components/Nav';
+import Main from './Components/Main';
+import Menu from './Components/Menu';
+import Footer from './Components/Footer';
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Main />
+      <Menu />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
